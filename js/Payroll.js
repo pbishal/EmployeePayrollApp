a939e4b8557d69c8e8ea5f9f17a20355b3bb9eb8
pbishal <pbishal60@gmail.com>
2021-06-06 00:11:02 +0530

    on form submit populated the employee payroll data object

--- js/Payroll.js
+++ js/Payroll.js
@@ -6,3 +6,45 @@ salary.addEventListener('input', function()
 {
     output.textContent = salary.value; 
 });
+// UC 9 PopulateOnSubmit
+const save = () => {
+    try {
+        let EmployeePayroll = createEmployeePayroll();
+    }
+    catch (e) {
+        return e;
+    }
+}
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayroll();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day')+","+getInputValueById('#month')+","+getInputValueById('#year');
+    employeePayrollData.startDate = new Date(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let sellItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            sellItems.push(item.value);
+    });
+    return sellItems;
+}
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
